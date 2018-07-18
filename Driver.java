eecb85729b693e6ec23f9ee3881c62c374606206
enochpowno <36739134+enochpowno@users.noreply.github.com>
2018-07-18 11:12:23 +0800

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,17 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CSVReader csvreader = new CSVReader();
+		
+		
+		System.out.println("1. Manila City");
+		System.out.println("2. Quezon City");
+		System.out.println("3. Makati City");
+		System.out.println("4. Pasay City");
+		System.out.println("5. Exit");
+		
+	}
+
+}
---
+++ mailManager.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class mailManager {
+	private ArrayList<postOffice> postOfficeList;
+	private ArrayList<location> locationList;
+	private ArrayList<destination> destinationList;
+	private ArrayList<distance> distanceList;
+	
+	private Mail mail;
+
+	public mailManager() {
+		
+		initializeComponents();
+		
+		
+	}
+	
+	public void initializeComponents() {
+		
+		
+		
+	}
+
+
+}
