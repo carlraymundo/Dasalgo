1855d1f894a4701011a70227709a44a92ca705e5
Carl James M. Raymundo <36413714+carlraymundo@users.noreply.github.com>
2018-07-21 20:45:36 +0800

    updated controller

---
+++ controller/DisplayPostOffice.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class DisplayPostOffice {
+
+    @FXML
+    private Button quezon;
+
+    @FXML
+    private Button makati;
+
+    @FXML
+    private Button pasay;
+
+    @FXML
+    private Button manila;
+
+}
