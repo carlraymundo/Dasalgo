2b3a29a8a8da9e91876f2e8b105a2a0f2ca0cbd1
enochpowno <enochpuno@yahoo.com>
2018-07-23 07:18:23 +0800

    Revert 3

---
+++ model/location.java
@@ -0,0 +1,40 @@
+
+
+public class location {
+	private String sPostNames, sLocation, sDestination, sDistance ;
+
+	public String getsPostNames() {
+		return sPostNames;
+	}
+
+	public void setsPostNames(String sPostNames) {
+		this.sPostNames = sPostNames;
+	}
+
+	public String getsLocation() {
+		return sLocation;
+	}
+
+	public void setsLocation(String sLocation) {
+		this.sLocation = sLocation;
+	}
+
+	public String getsDestination() {
+		return sDestination;
+	}
+
+	public void setsDestination(String sDestination) {
+		this.sDestination = sDestination;
+	}
+
+	public String getsDistance() {
+		return sDistance;
+	}
+
+	public void setsDistance(String sDistance) {
+		this.sDistance = sDistance;
+	}
+		
+	
+	
+}
---
+++ model/postOffice.java
@@ -0,0 +1,23 @@
+
+
+
+public class postOffice {
+	private String sPostOffice, sImage;
+	
+	
+	public postOffice(String sPostOffice) {
+		this.sPostOffice = sPostOffice;
+		
+	}
+	
+	public String getsPostOffice() {
+		return sPostOffice;
+		
+	}
+	
+	public void setsPostOffice(String sPostOffice) {
+		this.sPostOffice = sPostOffice;
+		
+	}
+	
+}
