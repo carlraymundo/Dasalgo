b68d3974146d559da97c7a3725e26c08ffe4a129
Carl James M. Raymundo <36413714+carlraymundo@users.noreply.github.com>
2018-07-23 23:03:52 +0800

    updated mail input

--- controller/MailInputs.java
+++ controller/MailInputs.java
@@ -1,15 +1,18 @@
 package controller;
 
+import java.util.ArrayList;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
+import model.Address;
+import model.CSVReader;
 
-public class MailInputs {
-
-    @FXML
-    private AnchorPane rootPane3;
+public class MailInputs{
 
     @FXML
     private TextField noOfMails;
@@ -24,7 +27,33 @@ public class MailInputs {
     private TextArea displayDestination;
 
     @FXML
+    private ComboBox<String> comboBox1;
+
+    @FXML
+    private Button Add;
+    
+    ArrayList<Address> AddressList = new ArrayList<Address>();
+    
+    
+    @FXML
     public void initialize() {
+    	CSVReader csvreader = new CSVReader();
+    	
+    	for(Address a:csvreader.readFile())
+			AddressList.add(a);
+    	
+    	ObservableList<String> addresses = FXCollections.observableArrayList();
+    	
+    	
+    	for(int i=0;i<AddressList.size();i++) {
+    		String location = AddressList.get(i).getDestination();
+    		addresses.add(location);
+    	}
+    	
+    	comboBox1.setItems(addresses);
     	
     }
+    
+    
+
 }
