c7f63a048da9ea64dfb22eadd54dd0cab581165a
Carl James M. Raymundo <36413714+carlraymundo@users.noreply.github.com>
2018-07-23 18:46:08 +0800

    updated model

---
+++ model/Address.java
@@ -0,0 +1,37 @@
+package model;
+
+
+public class Address {
+
+	private String postoffice;
+	private String location;
+	private String destination;
+	private float distance;
+	
+	
+	public String getPostoffice() {
+		return postoffice;
+	}
+	public void setPostoffice(String postoffice) {
+		this.postoffice = postoffice;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public float getDistance() {
+		return distance;
+	}
+	public void setDistance(float distance) {
+		this.distance = distance;
+	}
+	
+}
--- model/Mail.java
+++ model/Mail.java
@@ -1,18 +1,39 @@
 package model;
 
-
 public class Mail {
 
-
-	private String sImage;
+	private String mailLocation;
+	private String mailDestination;
+	private double mailDistance;
+	private int mailpriority;
 	
-	public Mail() {
-		
-		
+	
+	
+	public int getMailPriority() {
+		return mailpriority;
+	}
+	public void setMailPriority(int priority) {
+		this.mailpriority = priority;
+	}
+	public String getMailLocation() {
+		return mailLocation;
+	}
+	public void setMailLocation(String mailLocation) {
+		this.mailLocation = mailLocation;
+	}
+	public String getMailDestination() {
+		return mailDestination;
+	}
+	public void setMailDestination(String mailDestination) {
+		this.mailDestination = mailDestination;
+	}
+	public double getMailDistance() {
+		return mailDistance;
+	}
+	public void setMailDistance(double mailDistance) {
+		this.mailDistance = mailDistance;
 	}
 	
-
 	
 	
 }
-
