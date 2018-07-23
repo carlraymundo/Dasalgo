857aa6d611f5e268b1e4e40fb3bbd9a824cb237f
Carl James M. Raymundo <36413714+carlraymundo@users.noreply.github.com>
2018-07-24 00:10:37 +0800

    sort inlcuded

--- controller/MailInputs.java
+++ controller/MailInputs.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,6 +13,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import model.Address;
 import model.CSVReader;
+import model.Mail;
 
 public class MailInputs{
 
@@ -33,6 +36,7 @@ public class MailInputs{
     private Button Add;
     
     ArrayList<Address> AddressList = new ArrayList<Address>();
+    ArrayList<Mail> MailContainer = new ArrayList<Mail>();
     
     
     @FXML
@@ -54,6 +58,30 @@ public class MailInputs{
     	
     }
     
-    
+	public void display() {
+		for(int i=0;i<MailContainer.size();i++) {
+			System.out.println("Mail Location: " + MailContainer.get(i).getMailLocation());
+			System.out.println("Mail Destination: " + MailContainer.get(i).getMailDestination());
+			System.out.println("Mail Distance: " + MailContainer.get(i).getMailDistance());
+			System.out.println("Mail Priority: " + MailContainer.get(i).getMailPriority());
+		}
+		System.out.println();
+		System.out.println();
+	}
+	
+	public void sort() {
+		
+		Collections.sort(MailContainer, Comparator.comparing((Mail m)->m.getMailLocation()).thenComparing(new Comparator<Mail>() {
+			@Override
+			public int compare(Mail o1, Mail o2) {
+					if (o1.getMailDistance() > o2.getMailDistance()) 
+						return 1;
+	                else if (o1.getMailDistance() < o2.getMailDistance()) 
+	                	return -1;
+	                else 
+	                	return 0;
+			}
+		}));
+	}
 
 }
