6909706587d5005818d2b907087f813546ef4c68
Carl James M. Raymundo <36413714+carlraymundo@users.noreply.github.com>
2018-07-23 19:19:16 +0800

    transition from start to input mails

--- controller/DisplayPostOffice.java
+++ controller/DisplayPostOffice.java
@@ -1,7 +1,9 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import model.location;
 
 public class DisplayPostOffice {
 
@@ -16,5 +18,38 @@ public class DisplayPostOffice {
 
     @FXML
     private Button manila;
+    
+    private location Location = new location();
+    
+    
+    private MainController c = new MainController();
+    
+    @FXML
+    public void Manila(ActionEvent event) {
+    	Location.setPostName("Manila City");
+    	System.out.println(Location.getPostName());
+    	c.GetUserInput(event);
+    }
+    
+    @FXML
+    public void Quezon(ActionEvent event) {
+    	Location.setPostName("Quezon City");
+    	System.out.println(Location.getPostName());
+    	c.GetUserInput(event);
+    }
+   
+    @FXML
+    public void Makati(ActionEvent event) {
+    	Location.setPostName("Makati City");
+    	System.out.println(Location.getPostName());
+    	c.GetUserInput(event);
+    }
+    
+    @FXML
+    public void Pasay(ActionEvent event) {
+    	Location.setPostName("Pasay City");
+    	System.out.println(Location.getPostName());
+    	c.GetUserInput(event);
+    }
 
 }
