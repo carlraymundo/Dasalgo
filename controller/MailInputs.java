5a4261b50a63c62a856ac6a3586b8759eef4ea91
Carl James M. Raymundo <36413714+carlraymundo@users.noreply.github.com>
2018-07-24 10:12:04 +0800

    updated

--- controller/MailInputs.java
+++ controller/MailInputs.java
@@ -46,8 +46,6 @@ public class MailInputs{
     	for(Address a:csvreader.readFile())
 			AddressList.add(a);
     	
-    	display1();
-    	
     	ObservableList<String> addresses = FXCollections.observableArrayList();
     	
     	
@@ -58,18 +56,7 @@ public class MailInputs{
     	
     	comboBox1.setItems(addresses);
     }
-    
-    public void display1() {
-		for(int i=0;i<AddressList.size();i++) {
-				System.out.println("Post office: " + AddressList.get(i).getPostoffice());
-				System.out.println("Location: " + AddressList.get(i).getLocation());
-				System.out.println("Destination: " + AddressList.get(i).getDestination());
-				System.out.println("Distance: " + AddressList.get(i).getDistance());
-			}
-		System.out.println();
-		System.out.println();
-	}
-    
+        
 	public void display() {
 		for(int i=0;i<MailContainer.size();i++) {
 			System.out.println("Mail Location: " + MailContainer.get(i).getMailLocation());
@@ -86,8 +73,6 @@ public class MailInputs{
 		System.out.println("Selected input: " + selected);
 		for(int j=0;j<AddressList.size();j++) {
 			if(AddressList.get(j).getDestination().equalsIgnoreCase(selected)) {
-//				int found =j;
-//				System.out.println("value of j = " + found);
 				Mail mail = new Mail();
 				mail.setMailLocation(AddressList.get(j).getLocation());
 				mail.setMailDestination(AddressList.get(j).getDestination());
@@ -96,6 +81,8 @@ public class MailInputs{
 				MailContainer.add(mail);
 			}
 		}
+		//display();
+		sort();
 		display();
 	}
 	
